057ee2fc460c2da502760a390dee1dd7f86a1ce8
sabbas <braimah_abbas@yahoo.co.uk>
2019-09-29 13:11:50 +0100

    equal aand hashcode added to PurchaseOrder

--- src/main/java/domain/PurchaseOrder.java
+++ src/main/java/domain/PurchaseOrder.java
@@ -5,6 +5,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class PurchaseOrder {
 
@@ -38,5 +39,18 @@ public class PurchaseOrder {
 
     public void setCustomer(Customer customer) {
         this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PurchaseOrder that = (PurchaseOrder) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
