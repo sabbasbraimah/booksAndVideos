ca7b4406ca5f69213debaab6f0f77b9e04d249bf
seiduabbasbraimah <braimah_abbas@yahoo.co.uk>
2020-04-08 11:10:43 +0100

    first commit

---
+++ src/main/java/com/ecom/ecomapp/EcomappApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.ecomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomappApplication.class, args);
+    }
+
+}
